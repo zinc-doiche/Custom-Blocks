2e823d5cbf991053ab7057469da6e723c23e5f24
lc0709 <3dollee@gmail.com>
2023-10-15 17:01:38 +0900

    init

--- src/main/java/com/github/doiche/Main.java
+++ src/main/java/com/github/doiche/Main.java
@@ -2,6 +2,7 @@ package com.github.doiche;
 
 import com.github.doiche.block.command.AnimalFarmBlockCommand;
 import com.github.doiche.block.listener.BlockListener;
+import com.github.doiche.block.object.AnimalFarmBlocks;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,6 +16,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        AnimalFarmBlocks.read();
         AnimalFarmBlockCommand.register();
         getServer().getPluginManager().registerEvents(new BlockListener(), Main.getInstance());
     }
